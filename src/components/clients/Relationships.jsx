7c6a0a27feff23a50fa29311024dca6a6472fda6
Cosmas <pkiruicos@gmail.com>
2024-07-02 10:38:05 +0300

    Addition of Relationship Component

---
+++ src/components/clients/Relationships.jsx
@@ -0,0 +1,14 @@
+const Relationships =({clientId})=>{
+    console.log("This is "+clientId)
+    return (
+        <div className='client'>
+            This are the relationships
+            {clientId}
+          
+        </div>
+      )
+}
+
+
+
+export default Relationships
