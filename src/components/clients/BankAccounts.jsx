d9984f7f9d240d489d87c22fb9346afca70c26b5
Cosmas <pkiruicos@gmail.com>
2024-07-19 11:18:39 +0300

    Bank Account Modification

--- src/components/clients/BankAccounts.jsx
+++ src/components/clients/BankAccounts.jsx
@@ -5,53 +5,112 @@ import './bankaccount.css'
 
 import React, { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../../configs/Config';
+import { json } from 'react-router-dom';
 const BankAccounts = ({ clientId }) => {
-const [accountNumbers, setAccountNumbers]=useState([])
-const [accountType, setAccountType]=useState("")
-const [accountName, setAccountName]=useState("")
-const [accountNo, setAccountNo]=useState("")
-const [bankBranch,setAccountBankBranch]=useState("")
-const [showAddSection,setShowAddSection]=useState(false)
-useEffect(() => {
-    fetch(API_BASE_URL + "/getCodesByCodesetId/18")
-        .then((response) => response.json())
-        .then(data => {
-            if (Array.isArray(data)) {
-                setAccountNumbers(data);
-                
+    const [accountNumbers, setAccountNumbers] = useState([])
+    const [accountType, setAccountType] = useState("")
+    const [accountName, setAccountName] = useState("")
+    const [accountNo, setAccountNo] = useState("")
+    const [bankBranch, setAccountBankBranch] = useState("")
+    const [showAddSection, setShowAddSection] = useState(false)
+    const [savedBanks, setSavedBanks] = useState(false)
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchBranch, setSearchBranch] = useState('');
+    const [selectedBank, setSelectedBank] = useState("")
+    const [bankBranches, setBankBranches] = useState([]);
+    const [clientBankAccounts, setClientBankAccounts] = useState([]);
+    useEffect(() => {
+        fetch(API_BASE_URL + "/getClientBankaccounts/" + clientId)
+            .then((response) => response.json())
+            .then(data => {
+                if (Array.isArray(data)) {
+                    setClientBankAccounts(data);
+
+
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    useEffect(() => {
+        fetch(API_BASE_URL + "/getCodesByCodesetId/18")
+            .then((response) => response.json())
+            .then(data => {
+                if (Array.isArray(data)) {
+                    setAccountNumbers(data);
+
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+    }, []);
+    useEffect(() => {
+        fetch(API_BASE_URL + "/getAllbanks")
+            .then((response) => response.json())
+            .then(data => {
+                if (Array.isArray(data)) {
+                    setSavedBanks(data);
+
+
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    const filteredBanks = Array.isArray(savedBanks)
+        ? savedBanks.filter(bank => bank.bankName.toLowerCase().includes(searchTerm.toLowerCase()))
+        : [];
+
+    const filteredBankBranches = Array.isArray(bankBranches)
+        ? bankBranches.filter(branch => branch.bankBranch.toLowerCase().includes(searchBranch.toLowerCase()))
+        : [];
+
+
+    const handleBankSelection = (bankCode) => {
+        setSelectedBank(bankCode);
+
+        // Fetch the subsidiaries using the selected bankCode
+        fetch(API_BASE_URL + "/getAllBranchesByBankcode/" + bankCode)
+            .then(response => response.json())
+            .then(data => {
+                setBankBranches(data);
+
+            })
+            .catch(error => {
+                console.error('Error fetching subsidiaries:', error);
+            });
+    };
+
+    const handleAddaccountSection = () => {
+        setShowAddSection(true)
+    }
+    const handleSubmitAccount = (e) => {
+        e.preventDefault()
+        const bankCode = selectedBank
+        const newaccount = { clientId, accountNo, accountName, accountType, bankCode, bankBranch }
+        fetch(API_BASE_URL + "/addAccountNumber", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newaccount)
+        }).then(response => {
+            if (response.ok) {
+                setShowAddSection(false)
+                return response.json()
+
             }
-        }).catch((error) => {
-            console.log(error);
-        });
-}, []);
-const handleAddaccountSection=()=>{
-    setShowAddSection(true)
-}
- const handleSubmitAccount=(e)=>{
-    e.preventDefault()
-    const newaccount={clientId,accountNo,accountName,accountType,bankBranch}
-    fetch(API_BASE_URL + "/addAccountNumber",{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body:JSON.stringify(newaccount)
-    }).then(response=>{
-        if(response.ok){
-            setShowAddSection(false)
-            return response.json()
-            
-        }
-        else{
-            console.log("Error Occurred")
-        }
-    }).catch(error=>{
-        console.log(error)
-    })
-    
+            else {
+                console.log("Error Occurred")
+            }
+        }).catch(error => {
+            console.log(error)
+        })
 
 
- }
+
+    }
 
     return (
         <div className='client'>
@@ -60,62 +119,135 @@ const handleAddaccountSection=()=>{
                 </div>
                 <div className='actionholder'><EditOutlinedIcon className='editAccount' /></div>
                 <div className='actionholder'><RemoveCircleOutlineOutlinedIcon className='deleteAccount' /></div>
-                
-               
+
+
             </div>
-            {showAddSection &&<div className='AddaccountDetailsFields'>
+            <div className='accountDetails'>
+                <table className='table table-hover table-sm table-striped'>
+                    <thead>
+                        <tr>
+
+                            <th>Account Name</th>
+                            <th>Account Type</th>
+                            <th>Account Number</th>
+                            <th>Bank Code</th>
+                            <th>Bank Name</th>
+                            <th>Branch Code</th>
+                            <th> Branch Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientBankAccounts.map((accounts)=>(
+                            <tr>
+                            <td>{accounts.accountName}</td>
+                            <td>{accounts.accountType}</td>
+                            <td>{accounts.accountNo}</td>
+                            <td>{accounts.bankCode}</td>
+                            <td>{accounts.bankName}</td>
+                            <td>{accounts.branchCode}</td>
+                            <td>{accounts.bankBranch}</td>
+
+                        </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+
+            </div>
+            {showAddSection && <div className='AddaccountDetailsFields'>
 
 
                 <div class="form-group row">
                     <label for="staticEmail" className="col-sm-2 col-form-label">Account Type</label>
                     <div class="col-sm-10">
                         <select className="custom-select custom-select-sm" onChange={
-                            e=>setAccountType(e.target.value)
-                            }>
+                            e => setAccountType(e.target.value)
+                        } >
                             <option value="" selected>---select---</option>
-                            {accountNumbers.map(type=>(
-                            <option value={type.codeId}>{type.description}</option>
+                            {accountNumbers.map(type => (
+                                <option value={type.description}>{type.description}</option>
                             ))}
                         </select>
-                       
+
                     </div>
                 </div><br></br>
                 <div class="form-group row">
-                    <label  className="col-sm-2 col-form-label">Account Name</label>
+                    <label className="col-sm-2 col-form-label">Account Name</label>
                     <div class="col-sm-10">
                         <input type="text" onChange={
-                            e=>setAccountName(e.target.value)
-                            } className="form-control" />
+                            e => setAccountName(e.target.value)
+                        } className="form-control" />
                     </div>
                 </div><br></br>
                 <div class="form-group row">
                     <label for="staticEmail" className="col-sm-2 col-form-label">Account No</label>
                     <div class="col-sm-10">
                         <input type="text" onChange={
-                            e=>setAccountNo(e.target.value)
-                            } className="form-control" id="staticEmail" />
+                            e => setAccountNo(e.target.value)
+                        } className="form-control" id="staticEmail" />
                     </div>
                 </div><br></br>
+
+                <div class="form-group row">
+                    <label for="staticEmail" className="col-sm-2 col-form-label">Bank Name</label>
+                    <div class="col-sm-10">
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Search bank"
+                            value={searchTerm}
+                            onChange={e => {
+                                setBankBranches([])
+                                setSearchTerm(e.target.value)
+
+                            }}
+                        />
+                        <select className="custom-select custom-select-sm" onChange={
+                            e => handleBankSelection(e.target.value)
+                        } multiple>
+                            <option value="" selected>---select---</option>
+
+                            {filteredBanks.map((banks) => (
+                                <option value={banks.bankCode}>{banks.bankName}</option>
+                            ))}
+
+                        </select>
+
+                    </div>
+                </div><br></br>
+
                 <div class="form-group row">
                     <label for="staticEmail" className="col-sm-2 col-form-label">Bank Branch</label>
                     <div class="col-sm-10">
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Search bank"
+                            value={searchBranch}
+                            onChange={e => {
+                                setSearchBranch(e.target.value)
+
+                            }}
+                        />
                         <select className="custom-select custom-select-sm" onChange={
-                            e=>setAccountBankBranch(e.target.value)
-                            }>
+                            e => setAccountBankBranch(e.target.value)
+                        } multiple>
                             <option value="" selected>---select---</option>
-                           
-                            <option value="1">Bank 1</option>
-                            <option value="2">Bank 2</option>
-                            <option value="3">Bank 3</option>
+
+                            {filteredBankBranches.map((branch) => (
+                                <option value={branch.branchCode}>{branch.bankBranch}</option>
+                            ))}
+
                         </select>
-                       
+
                     </div>
                 </div><br></br>
+                <br></br>
                 <div class="form-group row">
-                    <button className="btn btn-primary"onClick={handleSubmitAccount}>Submit</button>
+                    <button className="btn btn-primary" onClick={handleSubmitAccount}>Submit</button>
 
                 </div><br></br>
-                
+
 
             </div>}
 
