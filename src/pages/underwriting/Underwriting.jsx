8bb496d9df85dbfc665279437f68df3dbd5f0e35
Cosmas <pkiruicos@gmail.com>
2024-08-08 09:30:49 +0300

    Create Cover changes

--- src/pages/underwriting/Underwriting.jsx
+++ src/pages/underwriting/Underwriting.jsx
@@ -7,14 +7,15 @@ import React, { useState } from 'react'
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
 import Tab from '@mui/material/Tab';
-import BuyCover from '../../components/underwriting/BuyCover'
+import BuyCover from '../../components/underwriting/CreateCover'
+import CreateCover from '../../components/underwriting/CreateCover'
 
 
 
 
 
 const Underwriting = () => {
-  const [value, setValue] = useState("buycover");
+  const [value, setValue] = useState("createCover");
   const handleTabChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -30,14 +31,13 @@ const Underwriting = () => {
             <TabContext value={value}>
               <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <TabList onChange={handleTabChange} aria-label="lab API tabs example">
-                  <Tab label="Buy Cover" value="buycover" />
+                  <Tab label="Create Cover" value="createCover" />
                   <Tab label="Policy Details" value="policydetails" />
 
                 </TabList>
               </Box>
-              <TabPanel value="buycover"><BuyCover/></TabPanel>
+              <TabPanel value="createCover"><CreateCover/></TabPanel>
               <TabPanel value="policydetails" ></TabPanel>
-
 
 
             </TabContext>
